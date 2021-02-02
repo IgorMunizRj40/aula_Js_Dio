91017e9887a27fc6ea225ede60a81a36afdc6337
Igor Muniz <igor.galdinomuniz@gmail.com>
2021-02-02 13:51:51 -0300

    Introdução

---
+++ js/main.js
@@ -0,0 +1,22 @@
+let nome = "Igor Muniz";
+let idade=33;
+let idade2=10;
+let frase = "Brasil e a melhor seleção do mundo!";
+let n1 = 20;
+let n2=30;
+
+//alert("Bem vindo " + nome + " idade " + idade + " anos");
+//alert(idade+idade2)
+
+
+console.log(nome);
+console.log(idade);
+console.log(idade2);
+console.log(frase.replace("Brasil", "Japão"));//replace altera 
+
+alert(frase.replace("Brasil", "Japão"));//replace no alert
+
+console.log(frase.toUpperCase());//coloca toda var(let) em caixa alta
+console.log(frase.toLowerCase());//caixa baixa em toda let
+console.log(n1+n2);//assim como manipulamos as strings podemos manipular os numeros
+console.log(n1*n2);
